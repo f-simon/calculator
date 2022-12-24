b275c7d72f9e9f25c52bc1ffdc13c94bacc698bd
fsimon <franklynsimon@comcast.net>
2022-12-24 17:19:05 -0500

    Added addition method for doubles

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@
 public class Calculator {
     public static public static void main(String[] args) {
         public int addition(int a, int b){
-            return a + b;
+            return b + a;
         }
 
     }
