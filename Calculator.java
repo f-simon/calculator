19afaad4e2499967d3f74be53c7e7634faad69f9
fsimon <franklynsimon@comcast.net>
2022-12-26 12:34:00 -0500

    cleanup code

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,13 @@
 public class Calculator {
     public static public static void main(String[] args) {
         public int addition(int a, int b){
-            return b + a;
+            return a + b;
         }
 		
-		public double addition (double a, double b) {
-		
-		   return a + b;
-		}
 
+		public double addition(double a, double b) {
+			return a + b;
+			
+		
     }
 }
